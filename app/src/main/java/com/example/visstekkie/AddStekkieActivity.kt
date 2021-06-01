633451769949c31ce5126ac1d02ad19dc238dfe3
jvw <jvw91@msn.com>
2021-06-01 21:25:58 +0200

    Working on addStekkie
    Finally figured out why the hell onSaveInstanceState was not being called. For some reason the Android devs thought it was a good idea to overload the onSaveInstanceState, which caused me to overwrite the wrong method. Thanks to SO i figured it out ffs.
    
    AddStekkieActivity.kt:
     - wrote the code to take a picture and display it in the activity.
    
    AndroidManifest.xml:
     - more permission stuff
    
    MainActivity.kt:
     - permission checks, need testing on other devices (emulator SDK 21+)

--- app/src/main/java/com/example/visstekkie/AddStekkieActivity.kt
+++ app/src/main/java/com/example/visstekkie/AddStekkieActivity.kt
@@ -1,37 +1,78 @@
 package com.example.visstekkie
 
 import android.content.Context
+import android.content.Intent
+import android.graphics.Bitmap
 import android.location.Location
 import android.location.LocationManager
 import android.os.Bundle
+import android.provider.MediaStore
 import android.util.Log
 import android.view.View
+import android.widget.ImageView
 import android.widget.TextView
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class AddStekkieActivity : AppCompatActivity() {
     private val TAG = "AddStekkieActivity"
     private val newStekkie = StekkieModel("", "", 0, 0.0, 0.0)
     private lateinit var stekkieLoc: TextView
+    private lateinit var stekkieImg: ImageView
+
+    private val IMAGE_CAPTURE_CODE = 1001
+    private var picBitMap: Bitmap? = null
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_addstekkie)
+        stekkieImg = findViewById(R.id.stekkie_img)
         stekkieLoc = findViewById(R.id.stekkie_loc)
+
         Log.d(TAG, "onCreate: has started.")
         setStekkieLocation()
-
 //        getIncommingIntent()
+    }
+
+    /**
+     * Because on some devices the orientation changes after taking a picture, we need
+     * to save the picture when the activity is rebuild. On rebuild 'onRestoreInstanceState' is called.
+     *
+     * Values in a activity are put in outState "automatically" accept for ImageViews.
+     */
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        //Save bitmap (containing img) to outState
+        outState.putParcelable("picBitMap", picBitMap)
+    }
+
+    /**
+     * See doc on OnSaveInstanceState
+     */
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+
+        //Check if picBitMap is set in SavedInstanceState
+        if (savedInstanceState.get("picBitMap") != null) {
+            picBitMap = savedInstanceState.get("picBitMap") as Bitmap?
+            stekkieImg.setImageBitmap(picBitMap)
+        }
+
+        if (picBitMap != null) {
+            stekkieImg.setImageBitmap(picBitMap)
+        }
     }
 
     /**
      * Sets the location of the new stekkie when called.
      */
     private fun setStekkieLocation() {
+        //Tap into GPS sensor
         val locationManager = getSystemService(Context.LOCATION_SERVICE) as LocationManager
         val providers: List<String> =  locationManager.getProviders(true)
         var location: Location? = null
+
+        //Check all providers if they have a value for location
         for (provider in providers) {
             try {
                 location = locationManager.getLastKnownLocation(provider)
@@ -43,6 +84,7 @@ class AddStekkieActivity : AppCompatActivity() {
             }
         }
 
+        //Set long and lat in newStekkie obj
         if (location != null) {
             newStekkie.latitude = location.latitude
             newStekkie.longitude = location.longitude
@@ -59,17 +101,35 @@ class AddStekkieActivity : AppCompatActivity() {
     }
 
     /**
-     *
+     * Activates the intent to create a picture which also calls for functions to
+     * set the image to the ImageView.
      */
     fun addPhoto(view: View) {
+        //Open default camera intent to make a picture
+        val cameraIntent = Intent(MediaStore.ACTION_IMAGE_CAPTURE)
+        startActivityForResult(cameraIntent, IMAGE_CAPTURE_CODE)
+    }
 
+    /**
+     * Get's called by Android when user takes picture.
+     */
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+        //called when image was captured from camera intent
+        if (requestCode == IMAGE_CAPTURE_CODE){
+            picBitMap = data?.getParcelableExtra("data")
+
+            //set image captured to image view
+            stekkieImg.setImageBitmap(picBitMap)
+            Log.d(TAG, "onActivityResult: Image set.")
+        }
     }
 
     /**
      *
      */
     fun createStekkie(view: View) {
-
+        //TODO
     }
 
 
--- app/src/main/java/com/example/visstekkie/MainActivity.kt
+++ app/src/main/java/com/example/visstekkie/MainActivity.kt
@@ -1,11 +1,16 @@
 package com.example.visstekkie
 
+import android.Manifest
+import android.app.Activity
 import android.content.Intent
+import android.content.pm.PackageManager
 import android.location.Location
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.widget.ActivityChooserView
+import androidx.core.app.ActivityCompat
 import androidx.recyclerview.widget.LinearLayoutManager
 import androidx.recyclerview.widget.RecyclerView
 
@@ -28,20 +33,33 @@ class MainActivity : AppCompatActivity(), OnItemClickListener {
         //Binding the llm and adapeter to the recyclerview
         stekkieRv.layoutManager = llm
         stekkieRv.adapter = stekkieAdapter
+
+        //TODO check for all permissions on start.
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.CAMERA) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.CAMERA), 111)
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_NETWORK_STATE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_NETWORK_STATE), 111)
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 111)
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_COARSE_LOCATION), 111)
+        }
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.WRITE_EXTERNAL_STORAGE), 111)
+        }
     }
 
     /**
      *
      */
     fun createStekkie(view: View) {
-        //TODO change to actually using a form and the camera
-        Toast.makeText(this, "CreateStekkie called", Toast.LENGTH_SHORT).show()
-
         val intent = Intent(this@MainActivity, AddStekkieActivity::class.java)
         startActivity(intent)
 
 //        stekkieAdapter.notifyItemInserted(index)
-        Toast.makeText(this, "CreateStekkie finished", Toast.LENGTH_SHORT).show()
     }
 
     override fun onItemClick(position: Int) {
